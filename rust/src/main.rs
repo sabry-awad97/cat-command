f32d628a1f8a79f3c6ab6c777ecb278b2b4dc391
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-26 13:51:30 +0200

    -n Number all output lines

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,13 +6,14 @@ use std::path::Path;
 const DEFAULT_BUF_SIZE: usize = 4096;
 
 fn main() -> io::Result<()> {
-    let mut options = Options::new();
+    let mut options = Options::default();
     let files = parse_args(&mut options, env::args().skip(1));
 
     let mut buffer = Vec::with_capacity(DEFAULT_BUF_SIZE);
     let stdout = io::stdout();
-    // ensure that the write operation is atomic (i.e., not interrupted by another write operation).
     let mut stdout = stdout.lock();
+
+    let mut line_number = 1;
 
     for file in files {
         let path = Path::new(&file);
@@ -21,10 +22,11 @@ fn main() -> io::Result<()> {
             let reader = BufReader::new(file);
             for line in reader.lines() {
                 let line = line?;
-                if options.number_nonempty_lines {
-                    write_line(&mut stdout, &line, &mut buffer, true)?;
+                if options.number_all_lines {
+                    write_line(&mut stdout, &line, &mut buffer, line_number)?;
+                    line_number += 1;
                 } else {
-                    write_line(&mut stdout, &line, &mut buffer, false)?;
+                    write_line(&mut stdout, &line, &mut buffer, 0)?;
                 }
             }
         } else {
@@ -37,12 +39,14 @@ fn main() -> io::Result<()> {
 
 struct Options {
     number_nonempty_lines: bool,
+    number_all_lines: bool,
 }
 
-impl Options {
-    fn new() -> Options {
-        Options {
+impl Default for Options {
+    fn default() -> Self {
+        Self {
             number_nonempty_lines: false,
+            number_all_lines: false,
         }
     }
 }
@@ -55,6 +59,7 @@ fn parse_args(options: &mut Options, args: impl Iterator<Item = String>) -> Vec<
             for ch in arg[1..].chars() {
                 match ch {
                     'b' => options.number_nonempty_lines = true,
+                    'n' => options.number_all_lines = true,
                     _ => {}
                 }
             }
@@ -70,15 +75,13 @@ fn write_line(
     stdout: &mut impl Write,
     line: &str,
     buffer: &mut Vec<u8>,
-    number_line: bool,
+    line_number: usize,
 ) -> io::Result<()> {
     buffer.clear();
-    if number_line && !line.trim().is_empty() {
-        writeln!(stdout, "{}: {}", 1, line)?;
+    if line_number > 0 {
+        writeln!(stdout, "{}: {}", line_number, line)?;
     } else {
         writeln!(stdout, "{}", line)?;
     }
     stdout.flush()
 }
-
-// 
